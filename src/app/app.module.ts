ee049d7c21d4f1d64dfc937cda3d3283a0b0a6a8
namleyolo <happybear02042000@gmail.com>
2018-03-22 09:24:05 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './marterial.module';
+import { FormsModule } from '@angular/forms';
+// import {MatSnackBarModule} from '@angular/material';
 
 
 @NgModule({
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    // MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
